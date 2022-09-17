01b6d26d80a20c029c0a6ec0627b676403d527e5
Jon Shaw <jonshaw199@gmail.com>
2022-09-17 07:45:26 -0700

    Fixing and cleaning up constructors and fixing divide by 0 when no intervalMs is specified (which means TimeEvent only executes once)

--- src/stateEnt/virtual/base/timeEvent/timeEvent.cpp
+++ src/stateEnt/virtual/base/timeEvent/timeEvent.cpp
@@ -48,46 +48,28 @@ int TECBArg::getMaxCbCnt()
   return maxCbCnt;
 }
 
-TimeEvent::TimeEvent()
+TimeEvent::TimeEvent() : startMs(0), intervalMs(0), maxCbCnt(0), cbCnt(0)
 {
-  startMs = 0;
-  intervalMs = 0;
-  maxCbCnt = 0;
-  cbCnt = 0;
   mode = TE_MODE_INACTIVE;
 }
 
-TimeEvent::TimeEvent(unsigned long long s, time_event_cb c)
+TimeEvent::TimeEvent(unsigned long long s, time_event_cb c) : startMs(s), intervalMs(0), cb(c), maxCbCnt(1), cbCnt(0)
 {
-  startMs = s;
-  cb = c;
-  maxCbCnt = 1;
+  mode = TE_MODE_INACTIVE;
 }
 
-TimeEvent::TimeEvent(unsigned long long s, time_event_cb c, unsigned long long i)
+TimeEvent::TimeEvent(unsigned long long s, time_event_cb c, unsigned long long i) : startMs(s), intervalMs(i), cb(c), cbCnt(0)
 {
-  startMs = s;
-  intervalMs = i;
-  cb = c;
   maxCbCnt = MAX_CB_CNT_INF;
+  mode = TE_MODE_INACTIVE;
 }
 
-TimeEvent::TimeEvent(unsigned long long s, time_event_cb c, unsigned long long i, int m)
+TimeEvent::TimeEvent(unsigned long long s, time_event_cb c, unsigned long long i, int m) : startMs(s), intervalMs(i), cb(c), maxCbCnt(m), cbCnt(0)
 {
-  startMs = s;
-  intervalMs = i;
-  cb = c;
-  maxCbCnt = m;
+  mode = TE_MODE_INACTIVE;
 }
 
-TimeEvent::TimeEvent(unsigned long long s, time_event_cb c, unsigned long long i, int m, int m2)
-{
-  startMs = s;
-  intervalMs = i;
-  cb = c;
-  maxCbCnt = m;
-  mode = m2;
-}
+TimeEvent::TimeEvent(unsigned long long s, time_event_cb c, unsigned long long i, int m, int m2) : startMs(s), intervalMs(i), cb(c), maxCbCnt(m), cbCnt(0), mode(m2) {}
 
 unsigned long long TimeEvent::getStartMs()
 {
@@ -135,7 +117,7 @@ bool TimeEvent::cbIfTimeAndActive(unsigned long long curMs)
   if (mode == TE_MODE_ACTIVE && isTime(curMs) && startMs && cb(TECBArg(curMs, startMs, intervalMs, cbCnt, maxCbCnt))) // Checking intervalMs here since default constructor doesnt even define cb; might need stub there to be safe
   {
     unsigned long long elapsedMs = curMs - startMs;
-    cbCnt = elapsedMs / intervalMs; // Setting cbCnt to expected value rather than just incrementing
+    cbCnt = intervalMs ? elapsedMs / intervalMs : cbCnt + 1; // Setting cbCnt to expected value rather than just incrementing (unless intervalMs is 0)
     return true;
   }
   return false;
